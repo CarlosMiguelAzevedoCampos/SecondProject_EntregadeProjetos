b690962b03b65e9e289fffb27efc71306e8bbd7d
Carlos Campos <carlosmiguelazevedocampos1996@hotmail.com>
2020-09-26 17:10:04 +0100

    git ignore

---
+++ CMA.ISMAI.Delivery.Logging.Log/Interface/ILoggingService.cs
@@ -0,0 +1,8 @@
+﻿namespace CMA.ISMAI.Delivery.Logging.Interface
+{
+    public interface ILoggingService
+    {
+        void Fatal(string message);
+        void Info(string message);
+    }
+}
---
+++ CMA.ISMAI.Delivery.Logging.Log/Service/LoggingService.cs
@@ -0,0 +1,26 @@
+﻿using CMA.ISMAI.Delivery.Logging.Interface;
+using Microsoft.Extensions.Logging;
+
+namespace CMA.ISMAI.Delivery.Logging.Service
+{
+    public class LoggingService : ILoggingService
+    {
+        private readonly ILogger<LoggingService> _logger;
+
+        // constructor
+        public LoggingService(ILogger<LoggingService> logger = null)
+        {
+            _logger = logger;
+        }
+
+        public void Fatal(string message)
+        {
+            _logger.LogError($"Please, take care of this as soon as possible! - {message}");
+        }
+
+        public void Info(string message)
+        {
+            _logger.LogInformation($"Just a information.., look! - {message}");
+        }
+    }
+}
